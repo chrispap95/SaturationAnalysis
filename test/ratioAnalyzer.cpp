fa3dff57dab3618303e6677bc9d92ee5011c3021
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-04-03 04:09:29 -0500

    Commit code for ratio analysis

--- test/ratioAnalyzer.cpp
+++ test/ratioAnalyzer.cpp
@@ -144,8 +144,8 @@ int main(int argc, char** argv){
     }
     outputFile->cd();
 
-    TH1F* h1 = new TH1F("h1","sat1",200,100,200);
-    TH1F* h2 = new TH1F("h2","sat1",200,100,200);
+    TH1F* h1 = new TH1F("h1","sat1",1000,100,200);
+    TH1F* h2 = new TH1F("h2","sat1",1000,100,200);
 
     /**********************************
     **  start event loop
