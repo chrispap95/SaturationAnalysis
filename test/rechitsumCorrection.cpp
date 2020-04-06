7011bc3165e0026c5e03b19e1c9b1d99a844ae70
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-04-06 16:00:53 -0500

    fix rechit correction

--- test/rechitsumCorrection.cpp
+++ test/rechitsumCorrection.cpp
@@ -288,6 +288,7 @@ int main(int argc, char** argv){
             **     - in positive endcap
             */
             if(!index && zh > 0 && dR < coneSize) {
+                rechitsum += lenergy;
                 if(lenergy>27.7 && lenergy<27.85){
                     // Format: (layer, waferU, waferV, cellU, cellV)
                     std::tuple<int, int, int, int, int> saturatedCell;
@@ -309,7 +310,6 @@ int main(int argc, char** argv){
                     saturatedList2.insert(saturatedCell);
                 }
                 else {
-                    rechitsum += lenergy;
                     rechitsum_corr += lenergy;
                 }
             }
