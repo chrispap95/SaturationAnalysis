c75ce8395a740b14250d3a8b653c1227d809387b
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-04-05 22:51:58 -0500

    Fix event branch type

--- test/sampleCreator.cpp
+++ test/sampleCreator.cpp
@@ -792,7 +792,7 @@ int main(int argc, char** argv){
                 MLdn4       = (*itr)[25];
                 MLdn5       = (*itr)[26];
                 MLdn6       = (*itr)[27];
-                MLevent     = event->at(0);
+                MLevent     = *event;
                 //MLrechitsum = rechitsumsaturated_Si;
                 MLsimHits   = (*itr)[30];
                 cellType    = (*itr)[31];
