94adddc40a5cebd13488325ca733105ce34bf8d9
Christos Papageorgakis <Christos.Papageorgakis@cern.ch>
2020-11-17 00:34:28 -0500

    Update script

--- regressionScripts/treeConverter.cpp
+++ regressionScripts/treeConverter.cpp
@@ -6,71 +6,71 @@
 **        ~ cells that are on the wafer edges
 */
 
-void treeConverter(TString input){
+void treeConverter(TString input, bool isTraining){
     TFile* f = TFile::Open(input+".root");
     TTree* t = dynamic_cast< TTree* >(f->Get("t1"));
     Float_t n1, n2, n3, n4, n5, n6, nup, ndown, rechit, layer, rechitsum;
     Float_t un1, un2, un3, un4, un5, un6, dn1, dn2, dn3, dn4, dn5, dn6;
-    Float_t saturated, simhits, cellType, event, cellU, cellV;
-
-    t->SetBranchAddress(     "MLlayer",     &layer );
-    t->SetBranchAddress(     "MLcellU",     &cellU );
-    t->SetBranchAddress(     "MLcellV",     &cellV );
-    t->SetBranchAddress(        "MLn1",        &n1 );
-    t->SetBranchAddress(        "MLn2",        &n2 );
-    t->SetBranchAddress(        "MLn3",        &n3 );
-    t->SetBranchAddress(        "MLn4",        &n4 );
-    t->SetBranchAddress(        "MLn5",        &n5 );
-    t->SetBranchAddress(        "MLn6",        &n6 );
-    t->SetBranchAddress(       "MLun1",       &un1 );
-    t->SetBranchAddress(       "MLun2",       &un2 );
-    t->SetBranchAddress(       "MLun3",       &un3 );
-    t->SetBranchAddress(       "MLun4",       &un4 );
-    t->SetBranchAddress(       "MLun5",       &un5 );
-    t->SetBranchAddress(       "MLun6",       &un6 );
-    t->SetBranchAddress(       "MLdn1",       &dn1 );
-    t->SetBranchAddress(       "MLdn2",       &dn2 );
-    t->SetBranchAddress(       "MLdn3",       &dn3 );
-    t->SetBranchAddress(       "MLdn4",       &dn4 );
-    t->SetBranchAddress(       "MLdn5",       &dn5 );
-    t->SetBranchAddress(       "MLdn6",       &dn6 );
-    t->SetBranchAddress(       "MLnup",       &nup );
-    t->SetBranchAddress(     "MLndown",     &ndown );
-    t->SetBranchAddress(    "cellType",  &cellType ); // 0 for 300um & 1 for 200um
-    t->SetBranchAddress(     "MLevent",     &event );
-    t->SetBranchAddress(   "MLsimHits",   &simhits );
-    t->SetBranchAddress( "MLrechitsum", &rechitsum );
-    t->SetBranchAddress( "MLsaturated", &saturated ); // saturated rechit value
+    Float_t saturated, simhits, thickness, event, cellU, cellV;
+    TString prefix = "ML"; // or for older code prefix = "ML"
+    t->SetBranchAddress( prefix+"layer", &layer );
+    t->SetBranchAddress( prefix+"cellU", &cellU );
+    t->SetBranchAddress( prefix+"cellV", &cellV );
+    t->SetBranchAddress( prefix+"n1", &n1 );
+    t->SetBranchAddress( prefix+"n2", &n2 );
+    t->SetBranchAddress( prefix+"n3", &n3 );
+    t->SetBranchAddress( prefix+"n4", &n4 );
+    t->SetBranchAddress( prefix+"n5", &n5 );
+    t->SetBranchAddress( prefix+"n6", &n6 );
+    t->SetBranchAddress( prefix+"un1", &un1 );
+    t->SetBranchAddress( prefix+"un2", &un2 );
+    t->SetBranchAddress( prefix+"un3", &un3 );
+    t->SetBranchAddress( prefix+"un4", &un4 );
+    t->SetBranchAddress( prefix+"un5", &un5 );
+    t->SetBranchAddress( prefix+"un6", &un6 );
+    t->SetBranchAddress( prefix+"dn1", &dn1 );
+    t->SetBranchAddress( prefix+"dn2", &dn2 );
+    t->SetBranchAddress( prefix+"dn3", &dn3 );
+    t->SetBranchAddress( prefix+"dn4", &dn4 );
+    t->SetBranchAddress( prefix+"dn5", &dn5 );
+    t->SetBranchAddress( prefix+"dn6", &dn6 );
+    t->SetBranchAddress( prefix+"nup", &nup );
+    t->SetBranchAddress( prefix+"ndown", &ndown );
+    t->SetBranchAddress( prefix+"thickness", &thickness );
+    t->SetBranchAddress( prefix+"event", &event );
+    t->SetBranchAddress( prefix+"simHits", &simhits );
+    t->SetBranchAddress( prefix+"rechitsum", &rechitsum );
+    t->SetBranchAddress( prefix+"saturated", &saturated );
 
     TFile* fout = new TFile(input+"_converted.root","RECREATE");
-    TTree* t1 = new TTree("t1","sample");
-    t1->Branch(     "layer",     &layer,     "layer/F" );
-    t1->Branch(        "n1",        &n1,        "n1/F" );
-    t1->Branch(        "n2",        &n2,        "n2/F" );
-    t1->Branch(        "n3",        &n3,        "n3/F" );
-    t1->Branch(        "n4",        &n4,        "n4/F" );
-    t1->Branch(        "n5",        &n5,        "n5/F" );
-    t1->Branch(        "n6",        &n6,        "n6/F" );
-    t1->Branch(       "dn1",       &dn1,       "dn1/F" );
-    t1->Branch(       "dn2",       &dn2,       "dn2/F" );
-    t1->Branch(       "dn3",       &dn3,       "dn3/F" );
-    t1->Branch(       "dn4",       &dn4,       "dn4/F" );
-    t1->Branch(       "dn5",       &dn5,       "dn5/F" );
-    t1->Branch(       "dn6",       &dn6,       "dn6/F" );
-    t1->Branch(       "un1",       &un1,       "un1/F" );
-    t1->Branch(       "un2",       &un2,       "un2/F" );
-    t1->Branch(       "un3",       &un3,       "un3/F" );
-    t1->Branch(       "un4",       &un4,       "un4/F" );
-    t1->Branch(       "un5",       &un5,       "un5/F" );
-    t1->Branch(       "un6",       &un6,       "un6/F" );
-    t1->Branch(       "nup",       &nup,       "nup/F" );
-    t1->Branch(     "ndown",     &ndown,     "ndown/F" );
-    t1->Branch(  "cellType",  &cellType,  "cellType/F" );
-    t1->Branch(     "event",     &event,     "event/F" );
-    t1->Branch(   "simhits",   &simhits,  "simmhits/F" );
-    t1->Branch( "rechitsum", &rechitsum, "rechitsum/F" );
-    t1->Branch(    "rechit",    &rechit,    "rechit/F" ); // Unsaturated rechit estimation
-    t1->Branch( "saturated", &saturated, "saturated/F" ); // Saturated rechit value
+    TTree* tree = new TTree("tree","sample");
+    tree->Branch( "layer", &layer, "layer/F" );
+    tree->Branch( "n1", &n1, "n1/F" );
+    tree->Branch( "n2", &n2, "n2/F" );
+    tree->Branch( "n3", &n3, "n3/F" );
+    tree->Branch( "n4", &n4, "n4/F" );
+    tree->Branch( "n5", &n5, "n5/F" );
+    tree->Branch( "n6", &n6, "n6/F" );
+    tree->Branch( "dn1", &dn1, "dn1/F" );
+    tree->Branch( "dn2", &dn2, "dn2/F" );
+    tree->Branch( "dn3", &dn3, "dn3/F" );
+    tree->Branch( "dn4", &dn4, "dn4/F" );
+    tree->Branch( "dn5", &dn5, "dn5/F" );
+    tree->Branch( "dn6", &dn6, "dn6/F" );
+    tree->Branch( "un1", &un1, "un1/F" );
+    tree->Branch( "un2", &un2, "un2/F" );
+    tree->Branch( "un3", &un3, "un3/F" );
+    tree->Branch( "un4", &un4, "un4/F" );
+    tree->Branch( "un5", &un5, "un5/F" );
+    tree->Branch( "un6", &un6, "un6/F" );
+    tree->Branch( "nup", &nup, "nup/F" );
+    tree->Branch( "ndown", &ndown, "ndown/F" );
+    tree->Branch( "thickness", &thickness, "thickness/F" );
+    tree->Branch( "event", &event, "event/F" );
+    tree->Branch( "simhits", &simhits, "simmhits/F" );
+    tree->Branch( "rechitsum", &rechitsum, "rechitsum/F" );
+    tree->Branch( "rechit", &rechit, "rechit/F" ); // Unsaturated rechit estimation
+    tree->Branch( "saturated", &saturated, "saturated/F" ); // Saturated rechit value
 
     for (int i = 0; i < t->GetEntries(); ++i) {
         t->GetEntry(i);
@@ -79,14 +79,14 @@ void treeConverter(TString input){
         ** mean  122.092 \pm 0.00015
         ** sigma 0.013
         */
-        //if(!cellType) rechit = 122.24305*simhits; // 300um cells
-        //if (cellType!=0 || layer<=0) continue; //only for training
-        //if (cellV==0 || cellV==15 || cellU==0 || cellU==15) continue; //only for training
-        //if (n1>27.7 || n2>27.7 || n3>27.7 || n4>27.7 || n5>27.7 || n6>27.7) continue; //only for training
-        //if (layer!=12) continue;
-        rechit = 122.090*simhits; // 300um cells
-        //else rechit = 182.13023*simhits;          // 200um cells
-        t1->Fill();
+        if (thickness == 300) rechit = 122.092*simhits; // 300um cells
+        else rechit = -1;
+
+        bool isHalfCell = (cellV==0 || cellV==15 || cellU==0 || cellU==15) ? 1 : 0;
+        bool hasSatNeighbor = (n1>27.7 || n2>27.7 || n3>27.7 || n4>27.7 || n5>27.7 || n6>27.7) ? 1 : 0;
+        if (!isTraining) tree->Fill();
+        else if (isHalfCell || hasSatNeighbor || thickness!=300 || layer<=0 || layer!=12) continue;
+        else tree->Fill();
     }
     fout->cd();
     fout->Write();
